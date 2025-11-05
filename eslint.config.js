8bc06b9b3caed49390dad3a9a53a4d6a53c51ab2
ghxartya <artemijgorbulin@gmail.com>
2025-11-05 19:20:03 +0200

    React Compiler + expanded ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import reactX from 'eslint-plugin-react-x'
+import reactDom from 'eslint-plugin-react-dom'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
@@ -11,13 +13,20 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      tseslint.configs.stylisticTypeChecked,
+      tseslint.configs.recommendedTypeChecked,
       reactHooks.configs['recommended-latest'],
+      reactX.configs['recommended-typescript'],
+      reactDom.configs.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
 ])
